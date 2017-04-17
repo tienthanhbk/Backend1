43af5b49aa840f534604d3519cea74aabbe9b590
Tien Thanh <tienthanh100696@gmail.com>
2017-04-17 16:03:01 +0700

    2 nd

--- app.js
+++ app.js
@@ -1,59 +1,63 @@
-console.log('Hello world');
-const fs = require('fs');
-//dung thu vien express
-const express = require('express');
+console.log('Hello nodemon');
 
+const fs = require('fs');
+//dung cai thu vien express
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const imagesController = require(__dirname + '/modules/images/imagesController');
 var app = express();
 
 //set public folder public
+//app.use(urlencoded)
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
   res.send('./public/index.html');
-});
+})
 
-app.get('/image/add', (req, res) => {
-  //Khai bao obj
+app.post('/image', (req, res) => {
+  //doc du lieu tu file imageData
+  var imageInfoCollection = imagesController.fetchImageCollection();
+  //khai bao object
   var imageInfo = {
-    name : req.query.name,
-    imageLink : req.query.imageLink,
-    description : req.query.description
+    name : req.body.name,
+    imageLink : req.body.imageLink,
+    description : req.body.description
   }
-  //lay du lieu tu file JSON
-  var data = fs.readFileSync("imageData.json",'utf-8');
-  var JSONdata = JSON.parse(data);
-  //Luu vao file
-  JSONdata.push(imageInfo);
-  fs.writeFileSync('imageData.json', JSON.stringify(JSONdata));
+  //push data moi vao colletiom
+  imageInfoCollection.push(imageInfo);
+
+  //luu lai vao file
+  imagesController.saveImageCollection(imageInfoCollection);
   //bao thanh cong
   res.send('Success');
-});
+})
 
-app.get('/image/get', (req, res) => {
-  //doc du lieu tu file JSON
-  var data = fs.readFileSync('imageData.json', 'utf-8');
-  var JSONdata = JSON.parse(data);
-  //
-  var html = "<html><head><meta charset='utf-8'><title></title></head><body>";
-  JSONdata.forEach(function(data, index){
-    var name = "Name: " + data.name + "</br>";
-    var img = "<img scr = '" + data.imageLink + "'>" + "</br>";
-    var des = "Description: " + data.description + "</br>";
-    var content = name + img + des;
-    console.log(img);
-    html += content;
-  });
-  html += "</body></html>";
-  res.send(html);
-});
+app.get('/image', (req,res) => {
+  var imageInfoCollection = imagesController.fetchImageCollection();
 
+  htmlString = '';
 
+  imageInfoCollection.forEach((data) => {
+    htmlString += `<div>${data.name}</div><img src="${data.imageLink}"><div>${data.description}</div>`;
+  })
 
-//mo 1 port chay local
-app.listen(6969, function(req, res) {
+  res.send(htmlString);
+
+})
+
+app.put('/image', (req, res) => {
+
+})
+
+app.delete('/image', (req, res) => {
+  
+})
+
+//mo 1 cai port de chay local
+app.listen(6969, (req, res) => {
   console.log('app listen on 6969');
-});
-
-// var funcName = () => {
-//
-// };
+})
